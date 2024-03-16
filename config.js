355989e40fbabfb0ea8d95db0bb17a0c48e468be
taro <110272356+tarowo@users.noreply.github.com>
2024-03-16 17:36:01 +0000

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ window.__CONFIG__ = {
   VITE_CORS_PROXY_URL: "https://harmonious-taffy-1062a0.netlify.app",
 
   // The READ API key to access TMDB
-  VITE_TMDB_READ_API_KEY: "6a3e7c4757b63dcdce0307ee4e8417fc",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2YTNlN2M0NzU3YjYzZGNkY2UwMzA3ZWU0ZTg0MTdmYyIsInN1YiI6IjY1ZjVkNjUwZDRkNTA5MDE3ZGFjMjgzNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.OBpgEvnu8-VD5XmZz3EhVoHwcaLu6TUbGgUhSmSKbX0",
 
   // The DMCA email displayed in the footer, null to hide the DMCA link
   VITE_DMCA_EMAIL: null,
