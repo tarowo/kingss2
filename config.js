7429dd4d8ddddcae31ea84c82f3d42097f2694a4
taro <110272356+tarowo@users.noreply.github.com>
2024-03-16 17:29:29 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 window.__CONFIG__ = {
   // The URL for the CORS proxy, the URL must NOT end with a slash!
   // If not specified, the onboarding will not allow a "default setup". The user will have to use the extension or set up a proxy themselves
-  VITE_CORS_PROXY_URL: "",
+  VITE_CORS_PROXY_URL: "https://harmonious-taffy-1062a0.netlify.app",
 
   // The READ API key to access TMDB
-  VITE_TMDB_READ_API_KEY: "",
+  VITE_TMDB_READ_API_KEY: "6a3e7c4757b63dcdce0307ee4e8417fc",
 
   // The DMCA email displayed in the footer, null to hide the DMCA link
   VITE_DMCA_EMAIL: null,
